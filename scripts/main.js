6ef1aa39e0bf94aff7ba0a2ae36d5985814f2570
Victor <victorlundhede@gmail.com>
2019-09-27 01:04:14 +0200

    Add lazy loading

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,53 @@ for (let link of nav_link) {
 		document.querySelector('.top_nav').classList.remove('open');
 	});
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+	let lazyloadImages;
+
+	if ('IntersectionObserver' in window) {
+		lazyloadImages = document.querySelectorAll('.lazy');
+		let imageObserver = new IntersectionObserver(function(entries) {
+			entries.forEach(function(entry) {
+				if (entry.isIntersecting) {
+					let image = entry.target;
+					image.src = image.dataset.src;
+					image.classList.remove('lazy');
+					imageObserver.unobserve(image);
+				}
+			});
+		});
+
+		lazyloadImages.forEach(function(image) {
+			imageObserver.observe(image);
+		});
+	} else {
+		let lazyloadThrottleTimeout;
+		lazyloadImages = document.querySelectorAll('.lazy');
+
+		function lazyload() {
+			if (lazyloadThrottleTimeout) {
+				clearTimeout(lazyloadThrottleTimeout);
+			}
+
+			lazyloadThrottleTimeout = setTimeout(function() {
+				let scrollTop = window.pageYOffset;
+				lazyloadImages.forEach(function(img) {
+					if (img.offsetTop < window.innerHeight + scrollTop) {
+						img.src = img.dataset.src;
+						img.classList.remove('lazy');
+					}
+				});
+				if (lazyloadImages.length === 0) {
+					document.removeEventListener('scroll', lazyload);
+					window.removeEventListener('resize', lazyload);
+					window.removeEventListener('orientationChange', lazyload);
+				}
+			}, 20);
+		}
+
+		document.addEventListener('scroll', lazyload);
+		window.addEventListener('resize', lazyload);
+		window.addEventListener('orientationChange', lazyload);
+	}
+});
