16cd96406cfcb0e0ef6e70fa537e1581810f9d7c
Victor <victorlundhede@gmail.com>
2019-09-27 17:35:40 +0200

    Add contact form validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,24 @@
+//SELECT ELEMENTS
 let elements = {
 	menu: document.querySelector('.menu_toggler'),
 	nav_link: document.querySelectorAll('.top_nav .nav_link'),
 	top_nav: document.querySelector('.top_nav'),
-	lazy: document.querySelectorAll('.lazy')
+	lazy: document.querySelectorAll('.lazy'),
+	submitBtn: document.querySelector('.submitBtn')
 };
-
+//EVENT LISTENER FOR WHEN USER CLICKS HAMBURGER MENU
 elements.menu.addEventListener('click', () => {
 	elements.menu.classList.toggle('open');
 	elements.top_nav.classList.toggle('open');
 });
-
+//LOOPS THROUGH NAV LINKS AND ADD EVENT LISTENER FOR WHEN USER CLICKS ON A MENU ITEM
 for (let link of elements.nav_link) {
 	link.addEventListener('click', () => {
 		elements.menu.classList.remove('open');
 		elements.top_nav.classList.remove('open');
 	});
 }
-
+//LAZY LOADING
 document.addEventListener('DOMContentLoaded', function() {
 	let lazyloadImages;
 
@@ -66,3 +68,74 @@ document.addEventListener('DOMContentLoaded', function() {
 		window.addEventListener('orientationChange', lazyload);
 	}
 });
+
+elements.submitBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	// Retrieving the values of form elements
+	let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let message = document.getElementById('message').value;
+
+	// Defining error variables with a default value
+	let nameErr = (emailErr = messageErr = true);
+
+	// Validate name
+	if (name == '') {
+		printError('nameErr', 'Please enter your name');
+	} else {
+		let regex = /^[a-zA-Z\s]+$/;
+		if (regex.test(name) === false) {
+			printError('nameErr', 'Please enter a valid name');
+		} else {
+			printError('nameErr', '');
+			nameErr = false;
+		}
+	}
+	// Validate email address
+	if (email == '') {
+		printError('emailErr', 'Please enter your email address');
+	} else {
+		// Regular expression for basic email validation
+		let regex = /^\S+@\S+\.\S+$/;
+		if (regex.test(email) === false) {
+			printError('emailErr', 'Please enter a valid email address');
+		} else {
+			printError('emailErr', '');
+			emailErr = false;
+		}
+	}
+	//Validate message
+	if (message == '') {
+		printError('messageErr', 'Please enter a valid message');
+	} else {
+		printError('messageErr', '');
+		messageErr = false;
+	}
+	// Prevent the form from being submitted if there are any errors
+	if ((nameErr || emailErr || messageErr) == true) {
+		return false;
+	} else {
+		// Creating a string from input data for preview
+		let dataPreview =
+			"You've entered the following details: \n" +
+			'Name: ' +
+			name +
+			'\n' +
+			'Email Address: ' +
+			email +
+			'\n' +
+			'Message: ' +
+			message +
+			'\n';
+		// Display input data in a dialog box before submitting the form
+		alert(dataPreview);
+		name.value = '';
+		email.value = '';
+		message.value = '';
+	}
+});
+
+// Defining a function to display error message
+function printError(elemId, hintMsg) {
+	document.getElementById(elemId).innerHTML = hintMsg;
+}
